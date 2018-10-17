fc2bc254f6dc4522cae4768c2cdb16cfde4dc6e1
blonski <piotrblonski96@gmail.com>
2018-10-17 19:17:20 +0200

    Fast Adjacency matrix

---
+++ TMP/Tmp/Tmp/Adjacency matrix.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMP
+{
+    class AdjacencyMatrix
+    {
+        private int[,] matrix;
+        public AdjacencyMatrix(int verticles)
+        {
+            matrix=new int[verticles,verticles];
+        }
+        private void correctPaths()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                matrix[i, i] = 0;
+            }
+        }
+        public void GenerateRandomMatrix()
+        {   //Random edges. This graph is always connected. Every Verticle is connected to each other.
+            // 0 means it is free path.
+            Random  numberGenerator= new Random();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                    matrix[i, k] = numberGenerator.Next(1,9);
+            }
+            correctPaths();
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                    Console.Write(matrix[i,k]+" ");
+                Console.WriteLine("");
+            }
+        }
+
+        
+    }
+}
--- TMP/Tmp/Tmp/Program.cs
+++ TMP/Tmp/Tmp/Program.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tmp
+namespace TMP
 {
     class Program
     {
+        static  AdjacencyMatrix _matrix;
         static void Main(string[] args)
         {
-            Console.WriteLine("Project init");
+            Console.WriteLine("Generated Matrix is:");
+            CreateMatrix(5);
             Console.ReadKey();
         }
+
+        private static void CreateMatrix(int verticles)
+        {
+            _matrix = new AdjacencyMatrix(verticles);
+            _matrix.GenerateRandomMatrix();
+            _matrix.print();
+        }
+
     }
 }
