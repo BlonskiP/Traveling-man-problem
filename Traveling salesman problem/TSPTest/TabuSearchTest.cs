596224dfe20b4490d26ac63b48541322634de6ea
blonski <piotrblonski96@gmail.com>
2018-12-11 21:19:34 +0100

    Better tabu?

--- Traveling salesman problem/TSPTest/TabuSearchTest.cs
+++ Traveling salesman problem/TSPTest/TabuSearchTest.cs
@@ -18,17 +18,40 @@ namespace TSPTest
 
             XDocument tspFile = XDocument.Load(file);
             AdjacencyMatrix testMatrix = new AdjacencyMatrix(tspFile);
-            TabuSearch test = new TabuSearch(testMatrix , 1000, 1);
-
+            TabuSearch test = new TabuSearch(testMatrix , 10000, 3);
             test.Solve();
             float Result1 = test.lowestCost;
-            float errRat = (Result1 - 2020) / 2020;
+            float errRat1 = (Result1 - 2020) / 2020;
+            test = new TabuSearch(testMatrix, 10000, 5);
+            test.Solve();
+            float Result2 = test.lowestCost;
+            float errRat2 = (Result2 - 2020) / 2020;
+            test = new TabuSearch(testMatrix, 10000, 7);
+            test.Solve();
+            float Result3 = test.lowestCost;
+            float errRat3 = (Result3 - 2020) / 2020;
+            test = new TabuSearch(testMatrix, 10000, 8);
+            test.Solve();
+            float Result4 = test.lowestCost;
+            float errRat4 = (Result4 - 2020) / 2020;
+            test = new TabuSearch(testMatrix, 10000, 10);
+            test.Solve();
+            float Result5 = test.lowestCost;
+            float errRat5 = (Result5 - 2020) / 2020;
+            test = new TabuSearch(testMatrix, 10000, 15);
+            test.Solve();
+            float Result6 = test.lowestCost;
+            float errRat6 = (Result6 - 2020) / 2020;
 
-            
-           
+            test = new TabuSearch(testMatrix, 10000, 15);
+            test.Solve();
+            float Result7 = test.lowestCost;
+            float errRat7 = (Result7 - 2020) / 2020;
 
-           
-            
+
+
+
+
 
 
         }
--- Traveling salesman problem/Traveling salesman problem/TabuSearch.cs
+++ Traveling salesman problem/Traveling salesman problem/TabuSearch.cs
@@ -33,20 +33,55 @@ namespace Traveling_salesman_problem
             bestSolution = new List<int>(curr);
             float currCost = _matrix.countCost(curr);
             lowestCost = currCost;
-            
+            int loop = 0;
             for(int i=0; i < _maxIterations; i++)
             {
+               
                 List<int> newBestNeig = findBestNeig(curr);
+
                 float newCost = _matrix.countCost(newBestNeig);
+
+                if (newCost == currCost) //if same result5x times
+                {
+                    loop++;
+
+                }
+                else { loop = 0; }
+
+                if(loop==5)
+                {
+                    loop = 0;
+                    bestSolution = new List<int>(newBestNeig);//remeber last best 
+                    curr = randomList();
+                    tabuMap.resetTabuMap();
+                    continue;
+
+                }
+              
                 curr = newBestNeig;
+                
                 currCost = newCost;
-                tabuMap.resetTabuMap();
+                tabuMap.decreseTabuLife();
             }
 
-            lowestCost = currCost;
+            lowestCost = _matrix.countCost(bestSolution);
 
         }
+        private List<int> randomList()
+        {
+            Random rnd = new Random();
+            List<int> newList = new List<int>();
+            newList.Add(0);
+            while(newList.Count<verticles)
+            {
+                int newVeticle = rnd.Next(1, verticles);
+                if (!newList.Contains(newVeticle))
+                    newList.Add(newVeticle);
 
+            }
+            newList.Add(0);
+            return newList;
+        }
         private List<int> findBestNeig(List<int> curr)
         {
             List<int> neig = new List<int>(curr);
@@ -62,22 +97,23 @@ namespace Traveling_salesman_problem
                 {
                     verticleA = i;
                     verticleB = j;
-                    if (!tabuMap.IsMoveTabu(verticleA, verticleB))
+                    if (!tabuMap.IsMoveTabu(verticleA, verticleB) || aspiration(bestScore,verticleA,verticleB,neig))
                     {
                         Swap<int>(neig, verticleA, verticleB);
                         currScore = _matrix.countCost(neig); 
                         if(currScore<bestScore)
                         {
-                            tabuMap.makeMoveATabu(verticleA, verticleB);
+                           
                             potencialNeig.Add(neig);
-                            neig = new List<int>(curr);
+                           
                         }
+                        neig = new List<int>(curr);
 
                     }
                     
 
                 }
-                tabuMap.decreseTabuLife();
+               
             }
 
             foreach(var neigbour in potencialNeig)
@@ -90,9 +126,31 @@ namespace Traveling_salesman_problem
                 }
             }
 
-
+            createTaboMove(neig,curr);
 
                     return neig;
+        }
+
+        private void createTaboMove(List<int> neig, List<int> curr)
+        {
+           for(int i=0;i<verticles;i++)
+            {
+                if(neig[i]!=curr[i])
+                {
+                    tabuMap.makeMoveATabu(neig[i], curr[i]);
+                    break;
+                }
+            }
+        }
+
+        private bool aspiration(float bestScore, int verticleA, int verticleB, List<int> neig)
+        {
+            List<int> newNeig = new List<int>(neig);
+            Swap<int>(newNeig, verticleA, verticleB);
+            float cost = _matrix.countCost(newNeig);
+            if (cost < 0.8*bestScore) return true;
+            return false;
+
         }
 
         private List<int> FindBestGreedySolution()
